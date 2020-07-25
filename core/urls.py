73c43105dc7da3e7d0940a3e3b17646513674a72
Italo Silvestre <italosilvestre3@icloud.com>
2020-07-25 16:11:05 -0300

    Create context

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home_page
+from .views import HomePageView, AboutPageView, ContactPageView
 urlpatterns = [
-        path('', home_page),
+        path('', HomePageView.as_view(), name="homepage"),
+        path('about/', AboutPageView.as_view(), name="about"),
+        path('contact/', ContactPageView.as_view(), name="contact"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,41 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-def home_page(request):
-    return render(request, "home_page.html")
+
+
+class HomePageView(TemplateView):
+
+    template_name = "home_page.html"
+
+    def get_context_data(self, **kwargs):
+        context = {
+            "title": "Página principal",
+            "content": "Bem-vindo a página principal"
+        }
+        return context
+
+class AboutPageView(TemplateView):
+
+    template_name = "about.html"
+
+    def get_context_data(self, **kwargs):
+        context = {
+            "title": "Página About",
+            "content": "Bem-vindo a página About"
+        }
+        return context
+
+class ContactPageView(TemplateView):
+
+    template_name = "contact.html"
+
+    def get_context_data(self, **kwargs):
+        context = {
+            "title": "Página Contato",
+            "content": "Bem-vindo a página de contato"
+        }
+        return context
+
+
+
+
+
